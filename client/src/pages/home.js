fffdeb68b71f9a034618700c55bf36e42791b4b4
Rajat <rajatraguvanshi3900@gmail.com>
2023-07-02 17:24:37 +0530

    added loader and reduced images loading time

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,17 +1,20 @@
 import { Recipecard } from "./recipecard";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Loader from "../components/Loader.js";
+
 export const Home = (props) => {
   const [recipes, setRecipes] = useState([]);
   const userID = window.localStorage.getItem("userID");
   const [savedRecipes, setSavedRecipes] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const fetchRecpies = async () => {
       try {
         const response = await axios.get(`${props.WEBSERVICE}/recipes`);
         setRecipes(response.data);
-        // console.log(response.data);
+        setIsLoaded(true);
       } catch (err) {
         console.error(err);
       }
@@ -36,27 +39,36 @@ export const Home = (props) => {
   }, []);
 
   const isSavedRecipePage = false;
-  return (
-    <section className="text-gray-800 body-font px-10 bg-gradient-to-r from-gray-300 to-gray-700 ">
-      <h1 className="font-Nunito text-7xl font-bold text-start py-8 underline underline-offset-4">
-        Top Recipes
-      </h1>
 
-      <div className="container py-8 mx-auto">
-        <div className="flex flex-wrap m-4">
-          {recipes.map((recipe) => {
-            return (
-              <Recipecard
-                key={recipe._id}
-                recipe={recipe}
-                savedRecipes={savedRecipes}
-                isSavedRecipePage={isSavedRecipePage}
-                setSavedRecipes={setSavedRecipes}
-              />
-            );
-          })}
+  if (isLoaded) {
+    return (
+      <section className="text-gray-800 body-font px-10 bg-gradient-to-r from-gray-300 to-gray-700 ">
+        <h1 className="font-Nunito text-7xl font-bold text-start py-8 underline underline-offset-4">
+          Top Recipes
+        </h1>
+
+        <div className="container py-8 mx-auto">
+          <div className="flex flex-wrap m-4">
+            {recipes.map((recipe) => {
+              return (
+                <Recipecard
+                  key={recipe._id}
+                  recipe={recipe}
+                  savedRecipes={savedRecipes}
+                  isSavedRecipePage={isSavedRecipePage}
+                  setSavedRecipes={setSavedRecipes}
+                />
+              );
+            })}
+          </div>
         </div>
+      </section>
+    );
+  } else {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <Loader />
       </div>
-    </section>
-  );
+    );
+  }
 };
--- client/src/pages/savedrecipe.js
+++ client/src/pages/savedrecipe.js
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Login } from "./login";
+import Loader from "../components/Loader";
+
 export const Savedrecipe = (props) => {
   const [cookie] = useCookies(["access_token"]);
 
   const [savedRecipes, setSavedRecipes] = useState([]);
   const userID = window.localStorage.getItem("userID");
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     const fetchSavedRecipes = async (req, res) => {
@@ -17,7 +20,7 @@ export const Savedrecipe = (props) => {
           `${props.WEBSERVICE}/recipes/savedrecipes/${userID}`
         );
         setSavedRecipes(response.data);
-        // console.log(response.data);
+        setIsLoaded(true);
       } catch (error) {
         console.log(error);
       }
@@ -36,43 +39,51 @@ export const Savedrecipe = (props) => {
   if (!cookie.access_token) {
     return <Login />;
   } else {
-    return (
-      <section className="text-gray-800 body-font px-10 min-h-screen">
-        <h1 className="font-Nunito text-7xl font-bold text-start py-8 underline underline-offset-4">
-          Your Saved Recipes
-        </h1>
+    if (isLoaded) {
+      return (
+        <section className="text-gray-800 body-font px-10 min-h-screen">
+          <h1 className="font-Nunito text-7xl font-bold text-start py-8 underline underline-offset-4">
+            Your Saved Recipes
+          </h1>
 
-        <div className="container py-8 mx-auto">
-          <div className="flex flex-wrap -m-4">
-            {savedRecipes.length === 0 ? (
-              <div className="flex flex-col justify-center mx-auto items-center space-y-4">
-                <h1 className="font-Nunito mx-auto font-bold text-xl text-center">
-                  You haven't saved any recipe yet.
-                </h1>
-                <button
-                  onClick={saveRecipeBtn}
-                  className="border-2 border-green-800 hover:bg-green-900 hover:border-green-900 bg-green-800 rounded-lg text-white font-Nunito text-base font-bold px-2 py-1"
-                >
-                  Save Recipes
-                </button>
-              </div>
-            ) : (
-              savedRecipes.map((recipe) => {
-                return (
-                  <Recipecard
-                    key={recipe._id}
-                    recipe={recipe}
-                    savedRecipes={savedRecipes}
-                    isSavedRecipePage={isSavedRecipePage}
-                    setSavedRecipes={setSavedRecipes}
-                  />
-                );
-              })
-            )}
+          <div className="container py-8 mx-auto">
+            <div className="flex flex-wrap -m-4">
+              {savedRecipes.length === 0 ? (
+                <div className="flex flex-col justify-center mx-auto items-center space-y-4">
+                  <h1 className="font-Nunito mx-auto font-bold text-xl text-center">
+                    You haven't saved any recipe yet.
+                  </h1>
+                  <button
+                    onClick={saveRecipeBtn}
+                    className="border-2 border-green-800 hover:bg-green-900 hover:border-green-900 bg-green-800 rounded-lg text-white font-Nunito text-base font-bold px-2 py-1"
+                  >
+                    Save Recipes
+                  </button>
+                </div>
+              ) : (
+                savedRecipes.map((recipe) => {
+                  return (
+                    <Recipecard
+                      key={recipe._id}
+                      recipe={recipe}
+                      savedRecipes={savedRecipes}
+                      isSavedRecipePage={isSavedRecipePage}
+                      setSavedRecipes={setSavedRecipes}
+                    />
+                  );
+                })
+              )}
+            </div>
           </div>
+        </section>
+      );
+    } else {
+      return (
+        <div className="flex justify-center items-center min-h-screen">
+          <Loader />
         </div>
-      </section>
-    );
+      );
+    }
   }
 };
 // DO CONDITIONAL RENDERING AT THE ABOVE DIV IN ORDER TO REMOVE THE SAVE BUTTON OF RECIPES CARDS SHOWING IN SAVED RECIPES
